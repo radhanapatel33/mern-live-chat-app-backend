c2333d985c5c11ee8a7af7df9fbf475823357da1
Radhana Patel <radhanapatel33@gmail.com>
2026-03-24 01:16:45 +0530

    cors set

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true
 }));
 
@@ -65,3 +65,5 @@ await connectDB();
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+
+
